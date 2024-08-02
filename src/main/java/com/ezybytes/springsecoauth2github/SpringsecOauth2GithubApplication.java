e42dc939c9eeec5e40685812634d3447367a41b5
JAEMIN <139306130+jm-jeong@users.noreply.github.com>
2024-08-02 18:17:56 +0900

    feat: github oauth2 client app 테스트용

---
+++ src/main/java/com/ezybytes/springsecoauth2github/SpringsecOauth2GithubApplication.java
@@ -0,0 +1,13 @@
+package com.ezybytes.springsecoauth2github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecOauth2GithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecOauth2GithubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ezybytes/springsecoauth2github/SpringsecOauth2GithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezybytes.springsecoauth2github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecOauth2GithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
